0f10b916f9ad2bf0156a7ff13d20c0c70c57810d
Stefan Hagen <stefan@hagen.link>
2020-12-08 22:41:45 +0100

    Create struct_id.cpp

---
+++ tests/struct_id.cpp
@@ -0,0 +1,21 @@
+#include "catch.hpp"
+#include "../reflect/hash.hpp"
+
+TEST_CASE("Struct identity compiles and is reproducible", "[positive]") {
+  constexpr const char* const FOO_SHAPE{
+      "Foo{"
+          "size_t const:version;"
+          "float:f;"
+          "double:d;"
+      "}"
+  };
+  struct Foo {
+      size_t const version{sdbm::hash_calculator<sdbm::length(FOO_SHAPE)>::calculate_value(FOO_SHAPE)};
+      float f{3.1415f};
+      double d{3.1415};
+  };
+  
+  // std::cout << "Foo --> " << Foo{}.version << " " << sdbm::length(FOO_SHAPE) << "\n";
+  // Foo --> 3738447584922188735 43
+  REQUIRE(Foo{}.version == 3738447584922188735);
+}
