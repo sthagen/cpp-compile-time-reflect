9708dc6aff864b04e713c5192260369fc824e07c
Stefan Hagen <stefan@hagen.link>
2020-12-08 22:39:05 +0100

    Create sdbm_hash.cpp

---
+++ tests/sdbm_hash.cpp
@@ -0,0 +1,8 @@
+#include "catch.hpp"
+#include "../reflect/hash.hpp"
+
+TEST_CASE("SDBM hash of string 42", "[positive]") {
+    constexpr const char* const fourtytwo{"42"};
+    size_t const hash_of_fourtytwo{sdbm::hash_calculator<sdbm::length(fourtytwo)>::calculate_value(fourtytwo)};
+    REQUIRE(hash_of_fourtytwo == 123);
+}
