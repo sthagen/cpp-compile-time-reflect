98ca4d871dc298f927ccd063183a5d297f4e0fe0
Stefan Hagen <stefan@hagen.link>
2020-12-08 22:51:54 +0100

    Update sdbm_hash.cpp

--- tests/sdbm_hash.cpp
+++ tests/sdbm_hash.cpp
@@ -4,5 +4,5 @@
 TEST_CASE("SDBM hash of string 42", "[positive]") {
     constexpr const char* const fourtytwo{"42"};
     size_t const hash_of_fourtytwo{sdbm::hash_calculator<sdbm::length(fourtytwo)>::calculate_value(fourtytwo)};
-    REQUIRE(hash_of_fourtytwo == 123);
+    REQUIRE(hash_of_fourtytwo == 3411198);
 }
