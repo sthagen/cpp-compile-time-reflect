4e9296729bf325631d9238051d484bc3fe0315ea
Stefan Hagen <stefan@hagen.link>
2020-12-08 22:55:29 +0100

    Update sdbm_hash.cpp

--- tests/sdbm_hash.cpp
+++ tests/sdbm_hash.cpp
@@ -6,3 +6,8 @@ TEST_CASE("SDBM hash of string 42", "[positive]") {
     size_t const hash_of_fourtytwo{sdbm::hash_calculator<sdbm::length(fourtytwo)>::calculate_value(fourtytwo)};
     REQUIRE(hash_of_fourtytwo == 3411198);
 }
+
+TEST_CASE("Length helper function yields 0 on nullptr", "[positive]") {
+    constexpr const char* const empty{0};
+    REQUIRE(sdbm::length(fourtytwo) == 0);
+}
